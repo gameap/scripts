db4daee4a4e25ae4f816cd25989e4a0d088de088
Nikita Kuznetsov <nkuznetsov@goodwix.team>
2021-03-12 20:03:36 +0300

    Add screen and tmux process-manager script

---
+++ process-manager/tmux/runner.sh
@@ -0,0 +1,285 @@
+#!/bin/bash
+
+set -u
+shopt -s dotglob
+
+[[ "${DEBUG:-}" == 'true' ]] && set -x
+export DEBIAN_FRONTEND="noninteractive"
+
+HISTORY_LIMIT=1000
+
+last_executed=""
+
+option_command="help"
+option_name=""
+option_dir=$(pwd)
+option_user=$(whoami)
+option_execute_command=""
+
+_parse_options() {
+    option_command=${1:-}
+    shift
+
+    local positional=()
+    while [[ $# -gt 0 ]]; do
+        local key="$1"
+
+        case $key in
+        -h | --help)
+            _show_help
+            exit 0
+            ;;
+        -d | --dir)
+            option_dir="$2"
+            shift
+            shift
+            ;;
+        -n | --name | --sname)
+            option_name="$2"
+            shift
+            shift
+            ;;
+        -u | --user)
+            option_user="$2"
+            shift
+            shift
+            ;;
+        -c | --command)
+            option_execute_command="$2"
+            shift
+            shift
+            ;;
+        *)
+            positional+=("$1")
+            shift
+            ;;
+        esac
+    done
+}
+
+_show_help() {
+    echo
+    echo 'Usage: ./runner.sh COMMAND [OPTIONS]'
+    echo 'Options:'
+    echo
+    echo 'Commands:'
+    echo '    start             Start new server'
+    echo '    stop              Stop running server'
+    echo '    restart           Restart server'
+    echo '    status            Server status'
+    echo '    get_console       Get output server log'
+    echo '    send_command      Send command into server'
+    echo
+    echo 'Options:'
+    echo
+    echo '    General options'
+    echo '          --dir, -d                   Working directory'
+    echo '          --name, --sname, -n         Session/Screen name option'
+    echo '          --user, -u                  Custom user'
+    echo
+    echo '    Start options'
+    echo '          --command, -c               Shell command'
+    echo
+    echo '    Send command into server'
+    echo '          --command, -c               Server command'
+    echo
+    echo 'Examples:'
+    echo '    Starting server'
+    echo '          ./runner.sh start -d /home/hl_server -n my_server -c "hlds_run -game valve +ip 127.0.0.1 +port 27015 +map crossfire"'
+    echo '          ./runner.sh start --dir /home/hl_server --name my_server --command "hlds_run -game valve +ip 127.0.0.1 +port 27015 +map crossfire"'
+    echo
+    echo '    Stopping server'
+    echo '          ./runner.sh stop -n my_server -u gameap'
+    echo
+    echo '    Sending command'
+    echo '          ./runner.sh send_command -n my_server -u gameap -c stats'
+    echo
+    echo '    Getting console output'
+    echo '          ./runner.sh get_console -n my_server -u gameap'
+}
+
+_run_command() {
+    local cmd="$*"
+    last_executed=$cmd
+
+    cd "${option_dir}" || return 1
+
+    if _run_as_user; then
+        if su "${option_user}" -c "${cmd}"; then
+            return 0
+        else
+            return 1
+        fi
+    else
+        if $cmd; then
+            return 0
+        else
+            return 1
+        fi
+    fi
+
+    cd - &> /dev/null || return 1
+}
+
+_run_as_user() {
+    if [[ -z ${option_user} ]]; then
+        return 1
+    fi
+
+    if [[ $(id -u) -ne "0" ]]; then
+        return 1
+    fi
+
+    if [[ $USER -ne ${option_user} ]]; then
+        return 0
+    fi
+
+    return 1
+}
+
+_server_start() {
+    if [[ $option_name == "" ]]; then
+        echo -e "Empty name" >> /dev/stderr
+        return 1
+    fi
+
+    if [[ ${option_execute_command} == "" ]]; then
+        echo -e "Empty command" >> /dev/stderr
+        return 1
+    fi
+
+    if _server_status; then
+        echo -e "Server is already running" >> /dev/stderr
+        return 1
+    else
+        if _run_command \
+            tmux \
+                set-option -g history-limit ${HISTORY_LIMIT} \; \
+                new-session -d -s "${option_name}" "${option_execute_command}"; then
+
+            return 0
+        else
+            return 1
+        fi
+    fi
+}
+
+_server_stop() {
+    if [[ $option_name == "" ]]; then
+        echo -e "Name empty"
+        return 1
+    fi
+
+    if _server_status; then
+        tmux kill-session -t "${option_name}"
+        return 0
+    else
+        echo "Couldn't find a running server" >> /dev/stderr
+        return 1
+    fi
+}
+
+_server_status() {
+    if _run_command tmux has-session -t "${option_name}" 2>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+_server_get_console() {
+    local line
+    local result
+    result=$(_run_command tmux capture-pane -p -t "${option_name}" -S-)
+
+    while read -r line
+    do
+        echo "$line"
+    done <<< "${result}"
+}
+
+_server_send_command() {
+    _run_command tmux send-keys -t "${option_name}" "${option_execute_command}" ENTER
+}
+
+_debug_command() {
+    echo -e "Command: \n" \
+        "cd ${option_dir}; ${last_executed}"
+}
+
+_tmux_check() {
+    if ! command -v tmux &> /dev/null; then
+        return 1
+    fi
+
+    return 0
+}
+
+main() {
+    if ! _tmux_check; then
+        echo -e "Tmux not found. Please install" >> /dev/stderr
+        return 1
+    fi
+
+    case ${option_command} in
+    start | run)
+        if _server_start; then
+            echo -e "Server started";
+
+            exit 0
+        else
+            echo -e "Server not started";
+            _debug_command
+
+            exit 1
+        fi
+        ;;
+
+    stop | kill)
+        if _server_stop; then
+            echo -e "Server stopped";
+            exit 0
+        else
+            echo -e "Server not stopped";
+            exit 1
+        fi
+        ;;
+
+    restart)
+        _server_stop >> /dev/null
+
+        if _server_start; then
+            echo -e "Server restarted"
+            exit 0
+        else
+            echo -e "Server not restarted"
+            exit 1
+        fi
+        ;;
+
+    status)
+        if _server_status; then
+           echo "Server is UP"
+           exit 0
+        else
+           echo "Server is Down"
+           exit 1
+        fi
+        ;;
+
+    get_console | output)
+        _server_get_console
+        ;;
+
+    send_command | input)
+        _server_send_command
+        ;;
+
+    *)
+        _show_help
+        ;;
+    esac
+}
+
+_parse_options "$@"
+main
